b116fc94e95989bee27909360af00cb25217cc4f
Sigma-UP <mynametrump@gmail.com>
2023-10-02 09:04:42 +0300

    fix: image

--- src/components/MainPage/MainPageHead/index.tsx
+++ src/components/MainPage/MainPageHead/index.tsx
@@ -1,12 +1,17 @@
 import { Avatar, Container, Details, Location, Name, Position } from "./index.styled";
 import ava from '../../../../public/img/ava.jpg'
-
+import { prefix
+ } from "../../../../utils/prefix";
 const MainPageHead = () => {
   return (
     <>
         <Container>
           <div>
-            <Avatar src={ava} alt='avatar'/>
+            <Avatar
+            src={prefix === '' ? ava : prefix + '/img/AVA.jpg'}
+            width={180} 
+            height={180} 
+            alt='avatar'/>
           </div>
           <Details>
             <Name>
