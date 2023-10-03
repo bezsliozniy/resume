33a8c515b6065f1ac0f1b426380115dd07db6927
Sigma-UP <mynametrump@gmail.com>
2023-10-04 02:41:55 +0300

    feat: bg-image and scrillbar

--- src/components/Layout/index.styled.ts
+++ src/components/Layout/index.styled.ts
@@ -3,7 +3,10 @@ import { breakpoints, colors } from "../../../styles/variables";
 
 export const AppWrapper = styled.div`
   min-height: 100vh;
-  background-color: ${colors.lightMint};
+  background-image: url('/img/bg2.png');
+  background-size: cover;
+  overflow: hidden;
+  /* background-color: ${colors.lightMint}; */
   color: ${colors.darkMint};
   display: flex;
   justify-content: center;
@@ -11,13 +14,26 @@ export const AppWrapper = styled.div`
 
 export const Content = styled.div`
   background-color: #fff;
-  /* background-color: ${colors.white}; */
+  overflow: auto;
+  height: 100vh; 
   width: 100%;
   max-width: 800px;
 
   display: flex;
   flex-direction: column;
   padding: 5px 90px;
+
+  &::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: ${colors.lightMint};
+  }
+  &::-webkit-scrollbar-track {
+      background-color: ${colors.white};
+  }
+
   @media (max-width: ${breakpoints.md}) {
     padding: 3px 30px;
   } 
